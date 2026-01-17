4c620daafd0ee7e26ae35f9edeefe41e10257f53
hell_y3ah <75150752+272-ykrf@users.noreply.github.com>
2026-01-17 17:54:55 +1000

    Add files via upload

---
+++ KR2_2_2_Efimov.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double square_sum(int n) {
+    if (n == 1) {
+        return sqrt(n + 1);
+    }
+    else {
+        return sqrt(n + 1) + square_sum(n - 1);
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << square_sum(n) << endl;
+}
